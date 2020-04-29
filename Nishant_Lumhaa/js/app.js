966ebdd766af5406965696622ae6bbe4df5d0ca0
Nishant Kumar <kumar.nishant2253@gmail.com>
2020-04-29 11:47:32 +0530

    my first commit
    sample

---
+++ Nishant_Lumhaa/js/app.js
@@ -0,0 +1,14 @@
+// (function () {
+//   const bannerForm = document.getElementById("bannerForm");
+//   const create = document.querySelector(".createbtn");
+//   const close = document.querySelector(".modalclose");
+//   const modal = document.querySelector(".bannerModal");
+
+//   create.addEventListener("click", function () {
+//     modal.classList.add("show");
+//   });
+
+//   close.addEventListener("click", function () {
+//     modal.classList.remove("show");
+//   });
+// })();
